0b834570cc2b82cd2b1ec1393115a365fba1e3a8
Hodo Mohamed <hmohamed0@gmail.com>
2014-01-14 12:46:50 -0500

    second commit

---
+++ exercise2.rb
@@ -0,0 +1,9 @@
+#tip for 55 dollar meal
+puts "A good tip for a 55 dollar meal would be $ #{55*0.18}. Don't be a cheapskate!"
+#adding a string & integar 
+puts "chips" + 5.to_s 
+#string interpolation of two numbers
+puts "#{45628 * 7839} 
+
+#boolean => true 
+(true && false) || (false && true) || !(false && false)
