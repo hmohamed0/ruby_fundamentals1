0b8d88e49c2c96a545dea132e6a811b68fee638e
Hodo Mohamed <hmohamed0@gmail.com>
2014-01-14 14:50:35 -0500

    second commit

--- exercise2.rb
+++ exercise2.rb
@@ -1,5 +1,5 @@
 #tip for 55 dollar meal
-puts "A good tip for a 55 dollar meal would be $ #{55*0.15}. Don't be a cheapskate!"
+puts "A good tip for a 55 dollar meal would be $ #{55*0.15}."
 #adding a string & integar 
 puts "coke" + 5.to_s 
 #string interpolation of two numbers
